031d601bdac64d908a06eeecc703a44f0b282645
mkocer <mustang5151@gmail.com>
2024-06-11 20:01:24 +0300

    .

---
+++ src/main/java/org/rd/Gezegen.java
@@ -0,0 +1,18 @@
+public enum Gezegen {
+    Gezegen1(1,1,1,1),
+    Gezegen2(2,2,2,2),
+    Gezegen3(3,3,3,3);
+
+    int siralama, uzaklik, yari_cap, donme_suresi;
+
+    Gezegen (int siralama, int uzaklik, int yari_cap, int donme_suresi){
+        this.siralama = siralama;
+        this.uzaklik = uzaklik;
+        this.yari_cap = yari_cap;
+        this.donme_suresi = donme_suresi;
+    }
+
+}
+
+
+
---
+++ src/main/java/org/rd/Main.java
@@ -0,0 +1,11 @@
+package org.rd;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(""+  );
+        for (Gezegen.gezegen : Gezegen.values()) {
+            System.out.println(gezegen +" siralama:"+ gezegen.siralama +" uzaklik:"+ gezegen.uzaklik +" yari cap:"+ gezegen.yari_cap +" donme suresi: "+ gezegen.donme_suresi);
+        }
+    }
+}
