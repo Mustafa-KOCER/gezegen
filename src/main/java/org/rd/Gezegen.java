e40fc7fb7040e14bc24fcdd90c8bc795c1282f70
mkocer <mustang5151@gmail.com>
2024-06-12 20:27:29 +0300

    .

--- src/main/java/org/rd/Gezegen.java
+++ src/main/java/org/rd/Gezegen.java
@@ -1,9 +1,15 @@
 package org.rd;
 
 public enum Gezegen {
-    Gezegen1(1,1,1,1),
-    Gezegen2(2,2,2,2),
-    Gezegen3(3,3,3,3);
+    Merkur(10,10,10,10),
+    Venüs(20,20,20,20),
+    Dünya(30,30,30,30),
+    Mars(40,40,40,40),
+    Jüpiter(50,50,50,50),
+    Satürn(60,60,60,60),
+    Uranüs(70,70,70,70),
+    Neptün(80,80,80,80),
+
 
     int siralama, uzaklik, yari_cap, donme_suresi;
 
